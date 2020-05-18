5a85663a7b95161e00f897d94316955b3509965b
ujjwalravi <59475060+ujjwalravi@users.noreply.github.com>
2020-05-19 00:52:29 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$('.responsive').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:"<button type='button' class='slick-prev pull-left'>PREV</button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'>NEXT</button>",
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
